2a9555eb8d88c8d1a11b050934264d36f9c0a3fb
SahilM1998 <63091402+SahilM1998@users.noreply.github.com>
2022-02-07 21:22:01 +0530

    Learn MERN

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
+require('../db/conn');
+const User = require('../model/userSchema');
+
 router.get('/', (req, res) => {
     res.send(`Hello world from the server from auth file`);
 });
 
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.json({ message: req.body });
-    // res.send(`my router page f registration`);
+
+    const { name, email, phone, work, password, cpassword } = req.body;
+
+    // console.log(req.body);
+    // console.log(name);
+    // res.json({ message: req.body });
+    // // res.send(`my router page f registration`);
+
+    if (!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({ error: "Please fill the reuired details" });
+
+    }
+
+    User.findOne({ email: email }).then((userExist) => {
+        if (userExist) {
+            return res.status(422).json({ error: "Email already exists" });
+
+        }
+
+        const user = new User({ name, email, phone, work, password, cpassword });
+
+        user.save().then(() => {
+            res.status(201).json({ message: "User registered successfully" });
+        }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
+
+    }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
+
+
+
 });
 
 module.exports = router;
