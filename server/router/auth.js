ea1a350bd585c577897f3b07efdce54eb8a0a7db
SahilM1998 <63091402+SahilM1998@users.noreply.github.com>
2022-02-08 21:44:04 +0530

    Learn Mern

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,21 +8,56 @@ router.get('/', (req, res) => {
     res.send(`Hello world from the server from auth file`);
 });
 
-router.post('/register', (req, res) => {
+
+// Using Promises
+// router.post('/register', (req, res) => {
+
+//     const { name, email, phone, work, password, cpassword } = req.body;
+
+//     // console.log(req.body);
+//     // console.log(name);
+//     // res.json({ message: req.body });
+//     // // res.send(`my router page f registration`);
+
+//     if (!name || !email || !phone || !work || !password || !cpassword) {
+//         return res.status(422).json({ error: "Please fill the reuired details" });
+
+//     }
+
+//     User.findOne({ email: email }).then((userExist) => {
+//         if (userExist) {
+//             return res.status(422).json({ error: "Email already exists" });
+
+//         }
+
+//         const user = new User({ name, email, phone, work, password, cpassword });
+
+//         user.save().then(() => {
+//             res.status(201).json({ message: "User registered successfully" });
+//         }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
+
+//     }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
+
+
+
+// });
+
+router.post('/register', async (req, res) => {
 
     const { name, email, phone, work, password, cpassword } = req.body;
 
     // console.log(req.body);
     // console.log(name);
     // res.json({ message: req.body });
-    // // res.send(`my router page f registration`);
+    // res.send(`my router page f registration`);
 
     if (!name || !email || !phone || !work || !password || !cpassword) {
         return res.status(422).json({ error: "Please fill the reuired details" });
 
     }
 
-    User.findOne({ email: email }).then((userExist) => {
+    try {
+        const userExist = await User.findOne({ email: email });
         if (userExist) {
             return res.status(422).json({ error: "Email already exists" });
 
@@ -30,13 +65,21 @@ router.post('/register', (req, res) => {
 
         const user = new User({ name, email, phone, work, password, cpassword });
 
-        user.save().then(() => {
-            res.status(201).json({ message: "User registered successfully" });
-        }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
+        // const userRegister = await user.save();
+        await user.save();
+        res.status(201).json({ message: "User registered successfully" });
 
-    }).catch((err) => res.status(500).json({ error: "Failed to registered" }));
+        // if (userRegister) {
+        //     res.status(201).json({ message: "User registered successfully" });
+        // }
+        // else {
+        //     res.status(500).json({ error: "Failed to registered" });
+        // }
 
-
+    }
+    catch (err) {
+        console.log(err);
+    }
 
 });
 
