3237ed6bb5ee50d52ebd2559a8c2bf11e6414d97
SahilM1998 <63091402+SahilM1998@users.noreply.github.com>
2022-02-21 20:29:13 +0530

    Learn Mern

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import signimage from "../images/login.svg";
 import { NavLink } from 'react-router-dom';
+import { useState } from "react";
 
 const Signup = () => {
+
+    const [user, setUser] = useState({
+        name: "", email: "", phone: "", work: "", password: "", cpassword: ""
+    });
+
+    let field, value;
+    const handleInputs = (e) => {
+        console.log(e);
+        field = e.target.name;
+        value = e.target.value;
+
+        setUser({ ...user, [field]: value });
+
+
+
+    }
+
     return (
         <>
             <section className='container'>
@@ -12,31 +30,37 @@ const Signup = () => {
                     </h2>
                     <form>
                         <label htmlFor="name">Name:</label><br /><br />
-                        <input type="text" name='name' id='name' autoComplete='off' placeholder='Your name' />
+                        <input type="text" name='name' id='name' autoComplete='off' placeholder='Your name'
+                            value={user.name} onChange={handleInputs} />
                         <br /><br />
 
                         <label htmlFor="email">Email:
                         </label><br /><br />
-                        <input type="email" name="email" id="email" autoComplete='off' placeholder='Your email' />
+                        <input type="email" name="email" id="email" autoComplete='off' placeholder='Your email'
+                            value={user.email} onChange={handleInputs} />
                         <br /><br />
 
                         <label htmlFor="phone"> Number:
                         </label><br /><br />
-                        <input type="number" name="phone" id="phone" autoComplete='off' placeholder='Your number' />
+                        <input type="number" name="phone" id="phone" autoComplete='off' placeholder='Your number'
+                            value={user.phone} onChange={handleInputs} />
                         <br /><br />
 
 
                         <label htmlFor="work">Profession:
                         </label><br /><br />
-                        <input type="text" name="work" id="work" autoComplete='off' placeholder='Your work details' />
+                        <input type="text" name="work" id="work" autoComplete='off' placeholder='Your work details'
+                            value={user.work} onChange={handleInputs} />
                         <br /><br />
 
                         <label htmlFor="password">Password:</label><br /><br />
-                        <input type="password" name="password" id="password" placeholder='Your password' />
+                        <input type="password" name="password" id="password" placeholder='Your password'
+                            value={user.password} onChange={handleInputs} />
                         <br /><br />
 
                         <label htmlFor="cpassword">Confirm Password:</label><br /><br />
-                        <input type="password" name="cpassword" id="cpassword" placeholder='Confirm password' />
+                        <input type="password" name="cpassword" id="cpassword" placeholder='Confirm password'
+                            value={user.cpassword} onChange={handleInputs} />
                         <br /><br />
 
                         <input type="submit" name='signup' id='signup' value="Register" />
